4ed2a92f6d1344c7cf357e3266d1288e23ac25bb
Anuj Saxena <anujsaxenaa@gmail.com>
2014-05-27 17:12:21 -0700

    added themes

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,18 +14,23 @@ DEFAULT_LANG = u'en'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
+THEME = "/Users/Anuj/pelican-themes/waterspill-en"
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
+LINKS =  (('Linkedin', 'https://www.linkedin.com/profile/view?id=115969517&trk=nav_responsive_tab_profile'),
+          ('Github', 'https://github.com/anujsaxenaa/'),
+          ('Kaggle', 'http://www.kaggle.com/users/117756/anuj-saxena/'),
           ('You can modify those links in your config file', '#'),)
+STATIC_PATHS = ['extra']
+EXTRA_PATH_METADATA = {
+'extra/CNAME': {'path': 'CNAME'}
+}
 
 # Social widget
+
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
